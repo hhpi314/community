7bbeaea2045430fc4c48d0f657c98d23d316b16f
zheng <zzzheng314@gmail.com>
2021-02-23 00:48:22 +0800

    增加 application.properties 变量配置

--- src/main/java/com/community/community/controller/AuthorizeController.java
+++ src/main/java/com/community/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.community.community.dto.AccessTokenDTO;
 import com.community.community.dto.GithubUser;
 import com.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,18 +14,25 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")  //确保来自"/callback"的http get请求被映射到callback()方法
     public String callback(@RequestParam(name="code")String code,    //参数绑定
                            @RequestParam(name="state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("9eb49da5d5bff303e60d");
+        accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_secret("8d3224e8172575ba9899021f85d031c07d4dc34d");
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getId());
+        System.out.println(user.getName());
         return "index";
     }
 }
