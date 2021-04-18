008970cfd362b976575627f0429a392dc88c16d5
zheng <zzzheng314@gmail.com>
2021-04-18 17:19:49 +0800

    完善问题编辑功能

--- src/main/java/com/community/community/controller/PublishController.java
+++ src/main/java/com/community/community/controller/PublishController.java
@@ -1,21 +1,39 @@
 package com.community.community.controller;
 
+import com.community.community.dto.QuestionDTO;
 import com.community.community.model.Question;
 import com.community.community.model.User;
 import com.community.community.repository.QuestionRepository;
+import com.community.community.service.QuestionService;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.jws.WebParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
     @Autowired
-    private QuestionRepository questionRepository;
+    private QuestionService questionService;
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id")Long id,
+                       Model model){
+        //根据id从数据库取得问题
+        QuestionDTO question = questionService.getById(id);
+        //将问题回显到页面上
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        //将id加入属性，方便传给更新函数
+        model.addAttribute("id",id);
+        return "publish";
+    }
 
     @GetMapping("/publish")
     public String publish(){
@@ -26,6 +44,7 @@ public class PublishController {
     public String doPublish(@Param("title")String title,
             @Param("description")String description,
             @Param("tag")String tag,
+            @Param("id")Long id,
             HttpServletRequest request, Model model){
 
         model.addAttribute("title", title);
@@ -55,10 +74,9 @@ public class PublishController {
         question.setDescription(description);
         question.setTag(tag);
         question.setCreator(user.getId());
-        question.setGmtCreate(System.currentTimeMillis());
-        question.setGmtModified(question.getGmtCreate());
-        questionRepository.save(question);
-
+        question.setId(id);//id可空
+        //questionRepository.save(question);
+        questionService.createOrUpdate(question);
         return "redirect:/";//没有异常返回首页
     }
 }
--- src/main/java/com/community/community/mapper/QuestionMapper.java
+++ src/main/java/com/community/community/mapper/QuestionMapper.java
@@ -4,6 +4,7 @@ import com.community.community.model.Question;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -21,4 +22,7 @@ public interface QuestionMapper {
 
     @Select("select count(1) from question where creator = #{userId}")
     Integer countByUserId(@Param(value = "userId") Integer userId);
+
+    @Update("update question set title = #{title}, description = #{description}, tag = #{tag}, gmt_modified = #{gmtModified} where id = #{id}")
+    void update(Question question);
 }
